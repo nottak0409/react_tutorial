72eb6037afbe1761c3f53d6f2320f9a7c2607638
nottak0409 <nottak0409@gmail.com>
2021-05-26 07:43:21 +0900

    手版の表示

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : '○';
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
@@ -37,7 +37,7 @@ class Board extends React.Component {
     }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
