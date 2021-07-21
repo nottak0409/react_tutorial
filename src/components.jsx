bd560b392a5eb9feae2642ceb2810e708ea8d3db
nottak0409 <nottak0409@gmail.com>
2021-07-22 07:18:02 +0900

    挙動チェック

--- src/components.jsx
+++ src/components.jsx
@@ -47,6 +47,7 @@ export class Game extends React.Component {
     render() {
         const history = this.props.history;
         const current = history[this.props.stepNumber];
+        console.log(current);
         const winner = calculateWinner(current.squares);
         const col = this.props.col;
         const row = this.props.row;
