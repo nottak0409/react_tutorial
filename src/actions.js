a1696f5fa65f8921b11ca1bd13e8e0bfa22da560
nottak0409 <nottak0409@gmail.com>
2021-07-23 17:30:54 +0900

    change actions events

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,17 @@
 export const CLICK_SQUARE = "CLICK_SQUARE";
 
-export function clickSquare(index) {
-    return { type: CLICK_SQUARE, index };
+export function clickSquare(payload) {
+    return { type: CLICK_SQUARE, payload };
 }
 
 export const JUMP_TO_PAST = "JUMP_TO_PAST";
 
-export function jumpToPast(index) {
-    return { type: JUMP_TO_PAST, index }
+export function jumpToPast(payload) {
+    return { type: JUMP_TO_PAST, payload }
+}
+
+export const GET_HISTORY = "GET_HISTORY";
+
+export function getHistory(payload) = {
+    return { type: GET_HISTORY, payload}
 }
